3cbfe9cdba9c5286d14f36cdde70c8f1572b1071
Jason Dude <jasondude16@gmail.com>
2024-03-21 18:24:52 -0500

    perform mice and apply one of imputed datasets to train, test and validation sets

--- src/process/03_impute_low_missing.R
+++ src/process/03_impute_low_missing.R
@@ -1,23 +1,59 @@
-
-library(micemd)
+library(mice)
 library(parallel)
-library(tidyverse)
+library(dplyr)
 
-setwd("/Users/Tanner/Library/CloudStorage/Box-Box/I2DB Datathon 2024/processed")
+# need to read in all data 
+df_train <- readRDS("./data/processed/Training_Set.RDS")
+df_valid <- readRDS("./data/processed/Validation_Set.RDS")
+df_test <- readRDS("./data/processed/Test_Sest.RDS")
 
-training <- readRDS("Training_Set.RDS")
+# create "type" variable so we know which set the row belongs to
+df_train$type <- "train"
+df_valid$type <- "valid"
+df_test$type <- "test"
+df_test$X1Yr_Death <- NA
 
-num_cores <- detectCores()
-use_cores <- num_cores - 1
+# combine all
+df_all <- rbind(
+  df_train,
+  df_valid,
+  df_test
+)
 
-cl <- makeCluster(use_cores)
+# remove vars that we don't want to use as predictors
+df_exclude <- df_all %>% select(Patient_ID, X1Yr_Death)
+df_sub <- df_all %>% select(-Patient_ID, -X1Yr_Death)
 
+# for the mice algorithm we can choose which rows are ignored during 
+# imputation, so we will ignore valid and test sets to avoid data leakage
+ignore_rows <- df_all$type %in% c("valid", "test")
 
-m_size <- 150
-n_iter <- 25
+# using multiple imputation with predictive mean matching 
+mice_imp_obj <- futuremice(
+  data = df_sub, 
+  m = 3, 
+  parallelseed = 16,
+  n.core = (detectCores()/2) - 2,
+  maxit = 5,
+  ignore = ignore_rows
+)
 
-imputed_data <- futuremice(Training_Set[,-1], m = m_size, maxit = n_iter, n.core = use_cores)
+# extract one of imputed datasets and add back vars
+df_imp <- complete(mice_imp_obj, 1)
+df_all <- bind_cols(df_exclude, df_imp)
 
-saveRDS(imputed_data, "Training_set_imputed_03212024.rds")
+# now re-split data into their respective sets and save
+for (type in c("train", "validation", "test")) {
+  df_imp %>% 
+    filter(type == type) %>% 
+    saveRDS(file.path("./data/processed/", paste0(type, "_set_imputed.RDS")))  
+}
 
-getwd()
+# also save mice object (for diagnostics)
+if (!dir.exists("./data/processed/supp")) {
+  dir.create("./data/processed/supp", recursive = TRUE)
+}
+saveRDS(mice_imp_obj, "./data/processed/supp/mice_object.rds")
+
+# clear workspace so we can run scripts in succession
+rm(list = ls())
