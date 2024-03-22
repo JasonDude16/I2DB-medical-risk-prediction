f00b53508a0f0da9bcf49ca5c8ab55f17c6ecfa4
Jason Dude <jasondude16@gmail.com>
2024-03-21 21:56:36 -0500

    average 50 imputed datasets

--- src/process/03_impute_low_missing.R
+++ src/process/03_impute_low_missing.R
@@ -11,6 +11,8 @@ df_test <- readRDS("./data/processed/Test_Sest.RDS")
 df_train$type <- "train"
 df_valid$type <- "valid"
 df_test$type <- "test"
+
+# also need to add NA variable
 df_test$X1Yr_Death <- NA
 
 # combine all
@@ -29,24 +31,43 @@ df_sub <- df_all %>% select(-Patient_ID, -X1Yr_Death)
 ignore_rows <- df_all$type %in% c("valid", "test")
 
 # using multiple imputation with predictive mean matching 
-mice_imp_obj <- futuremice(
-  data = df_sub, 
-  m = 3, 
-  parallelseed = 16,
-  n.core = (detectCores()/2) - 2,
-  maxit = 5,
-  ignore = ignore_rows
+mice_imp_obj <- df_sub %>% 
+  futuremice(
+    m = 50, 
+    parallelseed = 16,
+    n.core = (detectCores()/2) - 2,
+    maxit = 25,
+    ignore = ignore_rows
 )
 
-# extract one of imputed datasets and add back vars
-df_imp <- complete(mice_imp_obj, 1)
+# average imputed datasets
+# note that this would note not be appropriate if we were interested in
+# statistical estimates, but we made a judgment call that this is ok for ML purposes
+df_imp <- mice_imp_obj %>% 
+  complete(action = "long") %>%
+  select(-.imp) %>%
+  group_by(.id) %>%
+  summarize(
+    across(where(is.numeric), mean),
+    across(where(is.factor), .fns = ~ .x[1]),
+    across(where(is.character), .fns = ~ .x[1])
+  ) %>% 
+  select(-.id)
+
+# add back excluded vars
 df_all <- bind_cols(df_exclude, df_imp)
 
 # now re-split data into their respective sets and save
-for (type in c("train", "validation", "test")) {
-  df_imp %>% 
-    filter(type == type) %>% 
-    saveRDS(file.path("./data/processed/", paste0(type, "_set_imputed.RDS")))  
+for (type_ in c("train", "validation", "test")) {
+  
+  out <- file.path("./data/processed", paste0(type_, "_set_imputed.RDS"))
+  res <- df_all %>% filter(type == type_) %>% select(-type)
+  
+  # remove outcome that we added earlier
+  if (type_ == "test") {
+    res <- res %>% select(-X1Yr_Death)
+  }
+    saveRDS(res, out)  
 }
 
 # also save mice object (for diagnostics)
