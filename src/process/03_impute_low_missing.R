5f9f71bb2faae7c49758a0f90a54aec2af2f8747
Tanner Reece <tanner_reece@me.com>
2024-03-25 08:03:51 -0500

    Added PCA Report
    PCA on mice-averaged imputed continuous variables

--- src/process/03_impute_low_missing.R
+++ src/process/03_impute_low_missing.R
@@ -23,8 +23,8 @@ df_all <- rbind(
 )
 
 # remove vars that we don't want to use as predictors
-df_exclude <- df_all %>% select(Patient_ID, X1Yr_Death)
-df_sub <- df_all %>% select(-Patient_ID, -X1Yr_Death)
+df_exclude <- df_all %>% dplyr::select(Patient_ID, X1Yr_Death)
+df_sub <- df_all %>% dplyr::select(-Patient_ID, -X1Yr_Death)
 
 # for the mice algorithm we can choose which rows are ignored during 
 # imputation, so we will ignore valid and test sets to avoid data leakage
@@ -45,14 +45,14 @@ mice_imp_obj <- df_sub %>%
 # statistical estimates, but we made a judgment call that this is ok for ML purposes
 df_imp <- mice_imp_obj %>% 
   complete(action = "long") %>%
-  select(-.imp) %>%
+  dplyr::select(-.imp) %>%
   group_by(.id) %>%
-  summarize(
+  dplyr::summarize(
     across(where(is.numeric), mean),
     across(where(is.factor), .fns = ~ .x[1]),
     across(where(is.character), .fns = ~ .x[1])
   ) %>% 
-  select(-.id)
+  dplyr::select(-.id)
 
 # add back excluded vars
 df_all <- bind_cols(df_exclude, df_imp)
@@ -61,11 +61,11 @@ df_all <- bind_cols(df_exclude, df_imp)
 for (type_ in c("train", "validation", "test")) {
   
   out <- file.path("./data/processed", paste0(type_, "_set_imputed.RDS"))
-  res <- df_all %>% filter(type == type_) %>% select(-type)
+  res <- df_all %>% filter(type == type_) %>% dplyr::select(-type)
   
   # remove outcome that we added earlier
   if (type_ == "test") {
-    res <- res %>% select(-X1Yr_Death)
+    res <- res %>% dplyr::select(-X1Yr_Death)
   }
     saveRDS(res, out)  
 }
@@ -75,6 +75,7 @@ if (!dir.exists("./data/processed/supp")) {
   dir.create("./data/processed/supp", recursive = TRUE)
 }
 saveRDS(mice_imp_obj, "./data/processed/supp/mice_object.rds")
+
 
 # clear workspace so we can run scripts in succession
 rm(list = ls())
